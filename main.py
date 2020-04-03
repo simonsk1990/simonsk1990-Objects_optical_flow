39dea767e7c9d512453c0555f0b507ca168b3a86
Simon Shkilevich <sshkilevich@infinidat.com>
2020-04-03 05:29:00 -0700

    selecting roi works ok now

--- main.py
+++ main.py
@@ -1,59 +1,26 @@
 ##IMPORTS
-import numpy as np
 import cv2
+import modules
+import time
 import matplotlib.pyplot as plt
 
 
-roi = None
-def selectObject(event,x,y,flags,params):
-    global roi, frame
-    if event == cv2.EVENT_LBUTTONDOWN:
-        ret_now, frame = cap.read()
-        roi = cv2.selectROI(frame, False)
-        if roi!= None:
 
-            pass
-
-
-
+roi, img = modules.GettingLiveRoi()
+print('roi is {}'.format(roi))
 cap = cv2.VideoCapture(0)
 while True:
-    ret, frame = cap.read()
-    if ret == True:##This VM has drivers issues between host and VM
-        ##TODO Select Object to detect
-        cv2.imshow('stream', frame)
-        cv2.putText(frame, 'When ready - freeze the frame by pressing the mouse', (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
-        cv2.setMouseCallback('stream',selectObject)
-        cv2.destroyWindow('ROI selector')
-
-        print(roi)
-        tracker = cv2.TrackerMedianFlow_create()
-        ret = tracker.init(frame, roi)
-
-
-
-
-
-
+    ret, frame_now = cap.read()
+    cv2.putText(frame_now, 'Tracking - to exit press q', (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5,
+                (0, 0, 255), 1)
+    cv2.imshow('stream2',frame_now)
 
 
     if cv2.waitKey(10) & 0xff == ord('q'):
-        # press q to exit
         break
-
-
-
-
-#TODO Create tracking like
-#TODO color the tracking line for directions
-
-#Stop parameters
-## stop params
-
-
-
-
-
 cap.release()
+cv2.destroyWindow('stream2')
+
+
 
 
--- modules.py
+++ modules.py
@@ -1,4 +1,34 @@
 import cv2
-from main import frame
+
+roi = None
+roi_img = None
+def GettingLiveRoi():
+
+    close = 0
+    def selectObject(event,x,y,flags,params):
+        global roi, roi_img
+        if event == cv2.EVENT_LBUTTONDOWN:
+            cv2.putText(frame, 'select the object and press enter twice,', (20, 40), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
+            roi = cv2.selectROI(frame, False)
+            roi_img = frame[roi[1]:roi[1]+roi[3],roi[0]:roi[0]+roi[2]]
+            pass
 
 
+    cap = cv2.VideoCapture(0)
+    while True:
+        ret, frame = cap.read()
+        if ret == True:##This VM has drivers issues between host and VM
+            ##TODO Select Object to detect LIVE!
+            cv2.putText(frame, 'When ready - freeze the frame by pressing the mouse,', (20, 20), cv2.FONT_HERSHEY_DUPLEX, 0.5, (0, 0, 255), 1)
+            cv2.imshow('stream',frame)
+            cv2.setMouseCallback('stream',selectObject)
+            cv2.destroyWindow('ROI selector')
+            print(roi)
+            if roi != None:
+                close =1
+
+        if cv2.waitKey(10) & close ==1 :
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    return roi , roi_img
